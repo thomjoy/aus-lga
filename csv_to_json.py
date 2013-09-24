02306aace545ddd14958a38a424aac77a80e67cc
Thom Joy <thomjoy1984@gmail.com>
2013-09-25 01:58:41 +1000

    updates

--- csv_to_json.py
+++ csv_to_json.py
@@ -2,9 +2,11 @@ import csv
 from itertools import izip
 import json
 
-f = open( 'nsw.csv', 'r' )
+f = open( 'qld.csv', 'r' )
+jsonOut = open ('qld.json', 'w+')
+
 reader = csv.reader( f )
-keys = ( "id","name", "2001","2002","2003","2004","2005","2006","2007","2008","2009","2010","2011","2012","change % (2007-2012)","change no.","area","density" )
+keys = ( "id", "name", "2001","2002","2003","2004","2005","2006","2007","2008","2009","2010","2011","2012","change % (2007-2012)","change no.","area","density" )
 
 out = []
 for property in reader:
@@ -14,3 +16,4 @@ for property in reader:
     out += [ data ]
 
 print json.dumps(out)
+jsonOut.write(json.dumps(out));
