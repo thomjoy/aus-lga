e9e8a2b053e4b08dadbe6c8d7c3ec06e1d6f3810
Thom Joy <thomjoy1984@gmail.com>
2013-10-02 11:49:48 +1000

    adding indigenous data

--- csv_to_json.py
+++ csv_to_json.py
@@ -11,7 +11,7 @@ f = open(args[0], 'r')
 jsonOut = open(args[1], 'w+')
 
 reader = csv.reader( f )
-keys = ( "lga_id", "lga_name", "population", "score", "national_rank", "national_decile", "national_percentile", "state", "state_rank", "state_decile", "state_percentile" )
+keys = ( "lga_name", "number_aboriginal_tsi", "non_indigenous", "not_stated", "total", "indigenous_total_percentage" )
 
 out = []
 for property in reader:
@@ -20,5 +20,4 @@ for property in reader:
     out = [dict(zip(keys, property)) for property in reader]
     out += [ data ]
 
-print json.dumps(out)
 jsonOut.write(json.dumps(out));
