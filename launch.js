f7b10f917cba13635a0643eb0dd5abbf7b8e9457
Thom Joy <tjoy@ipowow.com>
2014-04-15 10:36:30 +1000

    update package.json

--- launch.js
+++ launch.js
@@ -2,7 +2,7 @@ var api = require('api'),
     util = require('util'),
     connect = require('connect');
 
-var port = 8888;
+var port = 7777;
 connect.createServer(connect.static(__dirname)).listen(port);
 util.puts('Static Server listening on :' + port);
 
